23df35e83441fc5b7f4672c36e60335e25ce8ef7
Hu Junfeng <junfeng.hu@2359media.com>
2014-11-30 01:39:50 +0800

    Skin custom tab bar
    The background of the custom tab bar is currently set to black color. It needs to be changed to transparent when the background of tab bar controller is added.

---
+++ Slocan/Slocan/Views/SLCTabBarButton.h
@@ -0,0 +1,13 @@
+//
+//  SLCTabBarButton.h
+//  Slocan
+//
+//  Created by Hu Junfeng on 30/11/14.
+//  Copyright (c) 2014 2359 Media Pte Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLCTabBarButton : UIButton
+
+@end
---
+++ Slocan/Slocan/Views/SLCTabBarButton.m
@@ -0,0 +1,17 @@
+//
+//  SLCTabBarButton.m
+//  Slocan
+//
+//  Created by Hu Junfeng on 30/11/14.
+//  Copyright (c) 2014 2359 Media Pte Ltd. All rights reserved.
+//
+
+#import "SLCTabBarButton.h"
+
+@implementation SLCTabBarButton
+
+- (UIEdgeInsets)titleEdgeInsets {
+    return UIEdgeInsetsMake(6, 14, 0, 0);
+}
+
+@end
